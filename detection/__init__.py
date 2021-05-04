325d087868887f8f3b60ebb6b74d308f3dce8457
janaSunrise <warriordefenderz@gmail.com>
2021-05-04 22:44:53 +0530

    Added image detection.

--- detection/__init__.py
+++ detection/__init__.py
@@ -1,4 +1,4 @@
 import cv2
 
 HAARCASCADE_CONFIG = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-IMAGE_PATH_PREFIX = "detection/images"
+IMAGE_PATH_PREFIX = "detection/images/"
---
+++ detection/image.py
@@ -0,0 +1,29 @@
+import os
+
+import cv2
+
+from . import IMAGE_PATH_PREFIX, HAARCASCADE_CONFIG
+
+# Define the variables
+IMAGE_NAME = "000.jpg"
+
+classifier = cv2.CascadeClassifier(HAARCASCADE_CONFIG)
+
+if __name__ == "__main__":
+    # Get the image
+    img = cv2.imread(IMAGE_PATH_PREFIX + IMAGE_NAME)
+
+    img_grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    detections = classifier.detectMultiScale(img_grayscale, scaleFactor=1.1, minNeighbors=6)
+
+    # Get the face co-ordinates
+    print(f"Co-ordinates:\n{detections}")
+
+    # Now let's plot the face co-ordinates
+    for x, y, w, h in detections:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (64, 224, 208), 2)
+
+    # Time to show the Image with detection zones.
+    cv2.imshow("Face detection", img)
+    cv2.waitKey()
